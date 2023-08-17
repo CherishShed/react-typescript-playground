a232797fea17b7b03e9e075ddf4433b30d45639e
CherishShed <c.shed2000@gmail.com>
2023-08-17 23:54:25 +0100

    polymorphism for components

--- src/components/customButton.tsx
+++ src/components/customButton.tsx
@@ -3,10 +3,10 @@ type ButtonProp = {
   label?: string;
   variant?: "primary" | "secondary";
   onclick?: (value: string | undefined) => void;
-};
+} & React.ComponentProps<"button">;
 
-export const Button = ({ variant, label, onclick }: ButtonProp) => {
-  const buttonVariant = variant ? variant : "primary";
+export const Button = ({ variant, label, onclick, ...rest }: ButtonProp) => {
+  const buttonVariant = variant || "primary";
   console.log(`class-with-${buttonVariant}`);
   return (
     <button
@@ -14,6 +14,7 @@ export const Button = ({ variant, label, onclick }: ButtonProp) => {
       onClick={() => {
         onclick ? onclick(buttonVariant) : null;
       }}
+      {...rest}
     >
       {label}
     </button>
